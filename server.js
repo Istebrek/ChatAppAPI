32adb931d63609962b79b31e1e48ab6f4f7c850f
Istebrek <istebrek.akram@outlook.com>
2025-01-22 13:05:29 +0100

    Frontend

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require ("express");
+const app = express();
+
+const PORT = procces.env.PORT || 3000;
+const cors = require ("cors");
+
+app.use(express.json());
+app.use(cors());
+
+let messages = [];
+let currentId = 1;
+
+app.get("/api/messages", (req, res) => {
+    console.log("Getting all messages");
+    res.json(messages)
+});
+
+app.post("/api/message", (req, res) => {
+    console.log("Sending message");
+    const {text} = req.body;
+
+    if (!text){
+        return res.status(400);
+    } 
+    
+    const newMessage = {
+        id: currentId++,
+        text,
+        createdAt: new Date()
+    };
+
+    messages.push(newMessage);
+    res.status(200).json(newMessage);
+
+});
+
+
+
+app.listen(PORT, () => { //man kan skipp måsvingarna här om man vill, ej nödvändigt
+    console.log("Listening to " + PORT);
+});
